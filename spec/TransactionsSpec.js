617b4f8fc147a7ae902f2515f634f4ce0ea1ee56
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-11-30 12:57:45 +0000

    test: Added test for multiple deposits pushing to object correctly.  Test pass

--- spec/TransactionsSpec.js
+++ spec/TransactionsSpec.js
@@ -11,6 +11,12 @@ describe('Transactions class', function() {
       expect(testtransactions.transactions).toEqual([{date: '10-01-2012', type: 'deposit', amount: 1000}])
     })
 
+    it('multiple deposits save to object correctly', function() {
+      testtransactions.deposit(1000, '10-01-2012')
+      testtransactions.deposit(7000, '18-01-2012')
+      expect(testtransactions.transactions).toEqual([{date: '10-01-2012', type: 'deposit', amount: 1000}, {date: '18-01-2012', type: 'deposit', amount: 7000}])
+    })
+
   })
 
 })
