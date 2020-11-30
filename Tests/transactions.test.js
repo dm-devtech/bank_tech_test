8194577fa59a32a12c9fcae8e59e11519533fe29
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-11-30 18:17:58 +0000

    Feat: print(statement) test initially passes with withdrawal

--- Tests/transactions.test.js
+++ Tests/transactions.test.js
@@ -6,7 +6,7 @@ describe('testing deposit function', () => {
     testTransactions.deposit(1000, '10-01-2012')
     expect(testTransactions.allTransactions[0]['date']).toBe('10-01-2012')
     expect(testTransactions.allTransactions[0]['type']).toBe('deposit')
-    expect(testTransactions.allTransactions[0]['amount']).toBe(1000)
+    expect(testTransactions.allTransactions[0]['amount']).toBe(1000.00)
   })
 
   test('tests multiple deposits', () =>{
@@ -15,10 +15,10 @@ describe('testing deposit function', () => {
     testTransactions.deposit(7000, '18-01-2012')
     expect(testTransactions.allTransactions[0]['date']).toBe('11-01-2012')
     expect(testTransactions.allTransactions[0]['type']).toBe('deposit')
-    expect(testTransactions.allTransactions[0]['amount']).toBe(2000)
+    expect(testTransactions.allTransactions[0]['amount']).toBe(2000.00)
     expect(testTransactions.allTransactions[1]['date']).toBe('18-01-2012')
     expect(testTransactions.allTransactions[1]['type']).toBe('deposit')
-    expect(testTransactions.allTransactions[1]['amount']).toBe(7000)
+    expect(testTransactions.allTransactions[1]['amount']).toBe(7000.00)
   })
 
 })
@@ -29,7 +29,7 @@ describe('testing withdraw function', () => {
     testTransactions.withdraw(500, '14-01-2012')
     expect(testTransactions.allTransactions[0]['date']).toBe('14-01-2012')
     expect(testTransactions.allTransactions[0]['type']).toBe('withdraw')
-    expect(testTransactions.allTransactions[0]['amount']).toBe(500)
+    expect(testTransactions.allTransactions[0]['amount']).toBe(500.00)
   })
 
   test('withdraw transaction is saved to transactions object', () =>{
@@ -38,10 +38,19 @@ describe('testing withdraw function', () => {
     testTransactions.withdraw(3000, '19-01-2012')
     expect(testTransactions.allTransactions[0]['date']).toBe('17-01-2012')
     expect(testTransactions.allTransactions[0]['type']).toBe('withdraw')
-    expect(testTransactions.allTransactions[0]['amount']).toBe(8000)
+    expect(testTransactions.allTransactions[0]['amount']).toBe(8000.00)
     expect(testTransactions.allTransactions[1]['date']).toBe('19-01-2012')
     expect(testTransactions.allTransactions[1]['type']).toBe('withdraw')
-    expect(testTransactions.allTransactions[1]['amount']).toBe(3000)
+    expect(testTransactions.allTransactions[1]['amount']).toBe(3000.00)
+  })
+
+})
+
+describe('testing print function', () => {
+  test('testing print format is correct', () =>{
+    const testTransactions = new Transactions;
+    testTransactions.withdraw(500, '14-01-2012')
+    expect(testTransactions.printStatement()).toBe('date || credit || debit || balance\n14-01-2012 || || 500.00 || -500.00')
   })
 
 })
--- src/Transactions.js
+++ src/Transactions.js
@@ -2,14 +2,30 @@ export default class Transactions {
 
   constructor() {
     this.allTransactions = []
+    this.printHeader = "date || credit || debit || balance\n"
+    this.statement = ""
   }
 
   deposit(amount, date) {
-    this.allTransactions.push({date: date, type: 'deposit', amount: amount})
+    this.allTransactions.push({date: date, type: 'deposit', amount: Number(amount.toFixed(2))})
   }
 
   withdraw(amount, date) {
-    this.allTransactions.push({date: date, type: 'withdraw', amount: amount})
+    this.allTransactions.push({date: date, type: 'withdraw', amount: Number(amount.toFixed(2))})
+  }
+
+  format() {
+    var i;
+    for (i = 0; i < this.allTransactions.length; i++) {
+      if(this.allTransactions[i]['type'] === 'withdraw') {
+        this.statement += `${this.allTransactions[i]['date']} || || ${this.allTransactions[i]['amount'].toFixed(2)} || -500.00`
+      }
+    }
+  }
+
+  printStatement() {
+    this.format()
+    return this.printHeader+this.statement
   }
 
 }
