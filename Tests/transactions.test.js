a57d87eb7b944436111454bdbae87a40f7fd98a3
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-11-30 16:32:47 +0000

    Test: added test to check second transaction enters the allTransactions object

--- Tests/transactions.test.js
+++ Tests/transactions.test.js
@@ -1,13 +1,21 @@
 import Transactions from '../src/Transactions.js'
 
-beforeEach(() => {
-  const testtransactions = new Transactions;
-});
-
   test('deposit transaction is saved to transactions object', () =>{
     const testTransactions = new Transactions;
     testTransactions.deposit(1000, '10-01-2012')
     expect(testTransactions.allTransactions[0]['date']).toBe('10-01-2012')
     expect(testTransactions.allTransactions[0]['type']).toBe('deposit')
     expect(testTransactions.allTransactions[0]['amount']).toBe(1000)
+  })
+
+  test('tests multiple deposits', () =>{
+    const testTransactions = new Transactions
+    testTransactions.deposit(2000, '11-01-2012')
+    testTransactions.deposit(7000, '18-01-2012')
+    expect(testTransactions.allTransactions[0]['date']).toBe('11-01-2012')
+    expect(testTransactions.allTransactions[0]['type']).toBe('deposit')
+    expect(testTransactions.allTransactions[0]['amount']).toBe(2000)
+    expect(testTransactions.allTransactions[1]['date']).toBe('18-01-2012')
+    expect(testTransactions.allTransactions[1]['type']).toBe('deposit')
+    expect(testTransactions.allTransactions[1]['amount']).toBe(7000)
   })
