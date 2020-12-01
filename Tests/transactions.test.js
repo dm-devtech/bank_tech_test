b20a159c64f7b4d75d7f5320c133b8c65ba51ee5
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-12-01 16:22:36 +0000

    Tests: updating tests matches to be ToEqual instead of ToBe for deeper match

--- Tests/transactions.test.js
+++ Tests/transactions.test.js
@@ -11,59 +11,59 @@ describe('testing Transactions class', () => {
   describe('testing deposit function', () => {
     it('deposit transaction is saved to transactions object', () =>{
       testTransactions.deposit(1000, '10-01-2012')
-      expect(testTransactions.allTransactions[0]['date']).toBe('10-01-2012')
-      expect(testTransactions.allTransactions[0]['type']).toBe('deposit')
-      expect(testTransactions.allTransactions[0]['amount']).toBe(1000.00)
+      expect(testTransactions.allTransactions[0]['date']).toEqual('10-01-2012')
+      expect(testTransactions.allTransactions[0]['type']).toEqual('deposit')
+      expect(testTransactions.allTransactions[0]['amount']).toEqual(1000.00)
     })
 
     test('tests multiple deposits', () =>{
       testTransactions.deposit(2000, '11-01-2012')
       testTransactions.deposit(7000, '18-01-2012')
-      expect(testTransactions.allTransactions[0]['date']).toBe('11-01-2012')
-      expect(testTransactions.allTransactions[0]['type']).toBe('deposit')
-      expect(testTransactions.allTransactions[0]['amount']).toBe(2000.00)
-      expect(testTransactions.allTransactions[1]['date']).toBe('18-01-2012')
-      expect(testTransactions.allTransactions[1]['type']).toBe('deposit')
-      expect(testTransactions.allTransactions[1]['amount']).toBe(7000.00)
+      expect(testTransactions.allTransactions[0]['date']).toEqual('11-01-2012')
+      expect(testTransactions.allTransactions[0]['type']).toEqual('deposit')
+      expect(testTransactions.allTransactions[0]['amount']).toEqual(2000.00)
+      expect(testTransactions.allTransactions[1]['date']).toEqual('18-01-2012')
+      expect(testTransactions.allTransactions[1]['type']).toEqual('deposit')
+      expect(testTransactions.allTransactions[1]['amount']).toEqual(7000.00)
     })
   })
 
   describe('testing withdraw function', () => {
     test('withdraw transaction is saved to transactions object', () =>{
       testTransactions.withdraw(500, '14-01-2012')
-      expect(testTransactions.allTransactions[0]['date']).toBe('14-01-2012')
-      expect(testTransactions.allTransactions[0]['type']).toBe('withdraw')
-      expect(testTransactions.allTransactions[0]['amount']).toBe(500.00)
+      expect(testTransactions.allTransactions[0]['date']).toEqual('14-01-2012')
+      expect(testTransactions.allTransactions[0]['type']).toEqual('withdraw')
+      expect(testTransactions.allTransactions[0]['amount']).toEqual(500.00)
     })
 
     test('withdraw transaction is saved to transactions object', () =>{
       testTransactions.withdraw(8000, '17-01-2012')
       testTransactions.withdraw(3000, '19-01-2012')
-      expect(testTransactions.allTransactions[0]['date']).toBe('17-01-2012')
-      expect(testTransactions.allTransactions[0]['type']).toBe('withdraw')
-      expect(testTransactions.allTransactions[0]['amount']).toBe(8000.00)
-      expect(testTransactions.allTransactions[1]['date']).toBe('19-01-2012')
-      expect(testTransactions.allTransactions[1]['type']).toBe('withdraw')
-      expect(testTransactions.allTransactions[1]['amount']).toBe(3000.00)
+      expect(testTransactions.allTransactions[0]['date']).toEqual('17-01-2012')
+      expect(testTransactions.allTransactions[0]['type']).toEqual('withdraw')
+      expect(testTransactions.allTransactions[0]['amount']).toEqual(8000.00)
+      expect(testTransactions.allTransactions[1]['date']).toEqual('19-01-2012')
+      expect(testTransactions.allTransactions[1]['type']).toEqual('withdraw')
+      expect(testTransactions.allTransactions[1]['amount']).toEqual(3000.00)
     })
   })
 
   describe('testing print function', () => {
     test('testing print format is correct with one withdrawal transaction', () =>{
       testTransactions.withdraw(500, '14-01-2012')
-      expect(testTransactions.getBankStatement()).toBe('date || credit || debit || balance\n14-01-2012 || || 500.00 || -500.00\n')
+      expect(testTransactions.getBankStatement()).toEqual('date || credit || debit || balance\n14-01-2012 || || 500.00 || -500.00\n')
     })
 
     test('testing print format is correct with one deposit transaction', () =>{
       testTransactions.deposit(2000, '11-01-2012')
-      expect(testTransactions.getBankStatement()).toBe('date || credit || debit || balance\n11-01-2012 || 2000.00 || || 2000.00\n')
+      expect(testTransactions.getBankStatement()).toEqual('date || credit || debit || balance\n11-01-2012 || 2000.00 || || 2000.00\n')
     })
 
     test('testing printed statement is correct with multiple transactions', () =>{
       testTransactions.deposit(1000, '10-01-2012')
       testTransactions.deposit(2000, '13-01-2012')
       testTransactions.withdraw(500, '14-01-2012')
-      expect(testTransactions.getBankStatement()).toBe('date || credit || debit || balance\n14-01-2012 || || 500.00 || 2500.00\n13-01-2012 || 2000.00 || || 3000.00\n10-01-2012 || 1000.00 || || 1000.00\n')
+      expect(testTransactions.getBankStatement()).toEqual('date || credit || debit || balance\n14-01-2012 || || 500.00 || 2500.00\n13-01-2012 || 2000.00 || || 3000.00\n10-01-2012 || 1000.00 || || 1000.00\n')
     })
   })
 
@@ -80,7 +80,7 @@ describe('testing Transactions class', () => {
       testTransactions.deposit(1000.50, '10-01-2012')
       testTransactions.deposit(2000.75, '13-01-2012')
       testTransactions.withdraw(500.20, '14-01-2012')
-      expect(testTransactions.getBankStatement()).toBe('date || credit || debit || balance\n14-01-2012 || || 500.20 || 2501.05\n13-01-2012 || 2000.75 || || 3001.25\n10-01-2012 || 1000.50 || || 1000.50\n')
+      expect(testTransactions.getBankStatement()).toEqual('date || credit || debit || balance\n14-01-2012 || || 500.20 || 2501.05\n13-01-2012 || 2000.75 || || 3001.25\n10-01-2012 || 1000.50 || || 1000.50\n')
     })
   })
 
