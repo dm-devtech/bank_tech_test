3e5dfdec642a48d743d97cc0e869975e38beab61
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-12-01 10:57:20 +0000

    Refactor: correcting first PrintStatement test name and renaming PrintStatement test file to be more descriptive

--- Tests/printStatement.test.js
+++ Tests/printStatement.test.js
@@ -2,7 +2,7 @@ import Transactions from '../src/Transactions.js'
 import PrintStatement from '../src/PrintStatement.js'
 
 describe('tests print class exists', () => {
-  test('deposit transaction is saved to transactions object', () =>{
+  test('newStatement is an instance of the PrintStatement class', () =>{
     const newStatement = new PrintStatement;
     expect(newStatement).toBeInstanceOf(PrintStatement)
   })
