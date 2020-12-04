59801b4059ea35b00590e4e23c6c88e370dee7b5
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-12-04 11:25:01 +0000

    Tests: removed tests that test state rather than behaviour

--- Tests/transactions.test.js
+++ Tests/transactions.test.js
@@ -8,48 +8,8 @@ describe('testing Transactions class', () => {
     testTransactions = new Transactions();
   });
 
-  describe('testing deposit function', () => {
-    it('deposit transaction is saved to transactions object', () =>{
-      testTransactions.deposit(1000, '10-01-2012')
-      expect(testTransactions.allTransactions[0]['date']).toEqual('10/01/2012')
-      expect(testTransactions.allTransactions[0]['type']).toEqual('deposit')
-      expect(testTransactions.allTransactions[0]['amount']).toEqual(1000.00)
-    })
-
-    test('tests multiple deposits', () =>{
-      testTransactions.deposit(2000, '11-01-2012')
-      testTransactions.deposit(7000, '18-01-2012')
-      expect(testTransactions.allTransactions[0]['date']).toEqual('11/01/2012')
-      expect(testTransactions.allTransactions[0]['type']).toEqual('deposit')
-      expect(testTransactions.allTransactions[0]['amount']).toEqual(2000.00)
-      expect(testTransactions.allTransactions[1]['date']).toEqual('18/01/2012')
-      expect(testTransactions.allTransactions[1]['type']).toEqual('deposit')
-      expect(testTransactions.allTransactions[1]['amount']).toEqual(7000.00)
-    })
-  })
-
-  describe('testing withdraw function', () => {
-    test('withdraw transaction is saved to transactions object', () =>{
-      testTransactions.withdraw(500, '14-01-2012')
-      expect(testTransactions.allTransactions[0]['date']).toEqual('14/01/2012')
-      expect(testTransactions.allTransactions[0]['type']).toEqual('withdraw')
-      expect(testTransactions.allTransactions[0]['amount']).toEqual(500.00)
-    })
-
-    test('withdraw transaction is saved to transactions object', () =>{
-      testTransactions.withdraw(8000, '17-01-2012')
-      testTransactions.withdraw(3000, '19-01-2012')
-      expect(testTransactions.allTransactions[0]['date']).toEqual('17/01/2012')
-      expect(testTransactions.allTransactions[0]['type']).toEqual('withdraw')
-      expect(testTransactions.allTransactions[0]['amount']).toEqual(8000.00)
-      expect(testTransactions.allTransactions[1]['date']).toEqual('19/01/2012')
-      expect(testTransactions.allTransactions[1]['type']).toEqual('withdraw')
-      expect(testTransactions.allTransactions[1]['amount']).toEqual(3000.00)
-    })
-  })
-
   describe('testing getStatement function', () => {
-    test('testing statement.print() is called', () =>{
+    test('testing newStatement.print() is called', () =>{
       const statement = new Statement()
       const spy = jest.spyOn(statement, 'print')
       const test = new Transactions(statement)
