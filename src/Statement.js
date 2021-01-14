d54688aa81ac35612e74f56308718c551099db83
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-01-14 12:26:39 +0000

    refactored decimal function in statement class

--- src/Statement.js
+++ src/Statement.js
@@ -2,19 +2,18 @@
 
 export default class Statement {
 
-  convertToDecimal(amount) {
+  toDecimal(amount) {
     return Number(amount).toFixed(2)
   }
 
   print(allTransactions, balancePerTransaction) {
     let [statementHeader, statementDetail] = ["date || credit || debit || balance\n", ""]
-
     for (let i = allTransactions.length - 1; i >= 0; i--) {
       const transaction = allTransactions[i];
       if(transaction.type === 'withdraw') {
-        statementDetail += `${transaction.date} || || ${this.convertToDecimal(transaction.amount)} || ${this.convertToDecimal(balancePerTransaction[i])}\n`
+        statementDetail += `${transaction.date} || || ${this.toDecimal(transaction.amount)} || ${this.toDecimal(balancePerTransaction[i])}\n`
       } else if(allTransactions[i]['type'] === 'deposit') {
-        statementDetail += `${transaction.date} || ${this.convertToDecimal(transaction.amount)} || || ${this.convertToDecimal(balancePerTransaction[i])}\n`
+        statementDetail += `${transaction.date} || ${this.toDecimal(transaction.amount)} || || ${this.toDecimal(balancePerTransaction[i])}\n`
       }
     }
     return statementHeader+statementDetail
